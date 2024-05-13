9917bcee37315e62e69bd20523c982bc010536e5
Jax <tnsahr2580@dreamus.io>
2024-05-13 16:51:43 +0900

    feat(RECO-2632): prompt 수정

--- my-app/packages/custom_prompt/custom_prompt/chain.py
+++ my-app/packages/custom_prompt/custom_prompt/chain.py
@@ -17,7 +17,7 @@ few_shot_prompt = FewShotPromptTemplate(
     prefix="""당신은 음악 플레이리스트 관련된 텍스트를 받으면, 해당 플레이리스트의 특징을 추출하는 역할을 합니다. 
                       당신의 목표는 주어진 플레이리스트의 특징을 추출하는 것입니다.
                       각각의 특징 값들은 한글로 표기 되어야 합니다. 
-                      주어진 텍스트는 주로 음악 플레이리스트에 대한 설명입니다.\n{format_instructions}\n{few_shot_prompt}\n{content}""",
+                      주어진 텍스트는 주로 음악 플레이리스트에 대한 설명입니다. \n{format_instructions} 여기 예시가 있습니다. """,
     suffix="{content}",
     input_variables=["content", "genre", "tempo", "instruments", "mood", "etc_keywords"],
     partial_variables={"format_instructions": parser.get_format_instructions()},
